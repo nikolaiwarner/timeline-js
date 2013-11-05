c49945310338e8cb61f1538ec2bdd4d07d41ec35
Andrei Miulescu <lusu777@gmail.com>
2013-11-05 20:17:56 +1100

    folder worries

--- package.js
+++ package.js
@@ -4,6 +4,6 @@ Package.describe({
 
 Package.on_use(function (api, where) {
   api.use('jquery', 'client');
-  api.add_files(['/js/storyjs-embed.js', '/js/timeline-min.js', '/js/timeline.css'], ['client']);
+  api.add_files(['js/storyjs-embed.js', 'js/timeline-min.js', 'css/*'], ['client']);
 });
 
