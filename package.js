5834d6c0432dd965d41353471da1d65b02032ce5
Andrei Miulescu <lusu777@gmail.com>
2013-11-05 19:59:09 +1100

    Changes to way i include packages

--- package.js
+++ package.js
@@ -3,8 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('/js/storyjs-embed.js', ['client']);
-  api.add_files('/js/timeline-min.js', ['client']);
-  api.add_files('/js/timeline.css', ['client']);
+  api.use('jquery', 'client');
+  api.add_files(['/js/storyjs-embed.js', '/js/timeline-min.js', '/js/timeline.css'], ['client']);
 });
 
